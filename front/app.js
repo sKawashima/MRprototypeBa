8e1e6026f19a63871e2851836c6acd68c3ca8b2b
sKawashima <bysk.333@gmail.com>
2018-09-25 16:45:48 +0900

    set sound players

--- front/app.js
+++ front/app.js
@@ -1,7 +1,13 @@
 import 'reset-css'
 import './style.sass'
 
+import E3 from './sound/E3.wav'
+import G3 from './sound/G3.wav'
 import A3 from './sound/A3.wav'
+import B3 from './sound/B3.wav'
+import C4 from './sound/C4.wav'
+import D4 from './sound/D4.wav'
+import E4 from './sound/E4.wav'
 
 import Tone from 'tone'
 
@@ -21,7 +27,13 @@ window.onload = () => {
   app.ctx = app.c.getContext('2d')
   app.ctx.beginPath()
   // setup tone
+  tone.E3 = new Tone.Player(E3).toMaster()
+  tone.G3 = new Tone.Player(G3).toMaster()
   tone.A3 = new Tone.Player(A3).toMaster()
+  tone.B3 = new Tone.Player(B3).toMaster()
+  tone.C4 = new Tone.Player(C4).toMaster()
+  tone.D4 = new Tone.Player(D4).toMaster()
+  tone.E4 = new Tone.Player(E4).toMaster()
   // setup methods
   app.c.addEventListener('touchstart', touchstart)
   app.c.addEventListener('touchmove', touchmove)
